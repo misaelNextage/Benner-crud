28ec13cac0613301bca133e4e7250b7fb57c858c
Misael Orelio <cassi.mo@benner.com.br>
2022-08-10 15:13:06 -0300

    Correção git ignore

--- MVVM/CRUD/NovoProduto.cs
+++ MVVM/CRUD/NovoProduto.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using WpfApp3.core;
-using WpfApp3.MVVM.Model;
-using WpfApp3.MVVM.View;
 
 namespace WpfApp3.MVVM.ViewModel
 {
