58f550b6895657cc51d304d4dea2b27edcfe516a
Misael Orelio <cassi.mo@benner.com.br>
2022-08-18 14:46:17 -0300

    limpeza, função que não estava senod usada

--- MVVM/View/PessoaView.xaml.cs
+++ MVVM/View/PessoaView.xaml.cs
@@ -22,33 +22,6 @@ namespace WpfApp3.MVVM.View
             InitializeComponent();
         }
 
-        public void salvar(object sender, RoutedEventArgs e)
-        {
-            /*Pessoa pessoa = new Pessoa();
-            pessoa.Nome = Nome.Text;
-            pessoa.Id = 1;
-            pessoa.Cpf = Cpf.Text;
-            pessoa.Endereco = Endereco.Text;*/
-
-
-            List<Pessoa> source = new List<Pessoa>();
-
-            using (StreamReader r = new StreamReader("pessoa.json"))
-            {
-                string json = r.ReadToEnd();
-                source = JsonSerializer.Deserialize<List<Pessoa>>(json);
-            }
-
-            /*source.Add(pessoa);*/
-
-            string jsonString = JsonSerializer.Serialize(source, new JsonSerializerOptions() { WriteIndented = true });
-            using (StreamWriter outputFile = new StreamWriter("pessoa.json"))
-            {
-                outputFile.WriteLine(jsonString);
-                outputFile.Close();
-            }
-        }
-
         private void Pesquisar_TextChanged(object sender, TextChangedEventArgs e)
         {
 
